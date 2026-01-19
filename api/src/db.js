9751aaeb1894f9a823094c8fbc9803763eb1b684
LucianoBenCode <luciano.benitez.code@gmail.com>
2026-01-18 22:24:38 -0300

    TEsting

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,8 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipe } = sequelize.
+models;
 
 // Aca vendrian las relaciones
 //Comentarios
